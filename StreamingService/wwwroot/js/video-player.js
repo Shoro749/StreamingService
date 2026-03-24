676b0548ecd7f7a519d02e50734c604dd8b3f2f7
Rudnev Vyacheslav <v.rudnev31@gmail.com>
2026-03-25 00:13:56 +0200

    added player buttons

--- StreamingService/wwwroot/js/video-player.js
+++ StreamingService/wwwroot/js/video-player.js
@@ -1,3 +1,13 @@
 ﻿document.addEventListener('DOMContentLoaded', () => {
     const player = new Plyr('#main-player');
+
+    const backBtn = document.getElementById('back-link');
+
+    player.on('pause', () => {
+        backBtn.classList.add('!opacity-100')
+    });
+
+    player.on('play', () => {
+        backBtn.classList.remove('!opacity-100')
+    });
 });
