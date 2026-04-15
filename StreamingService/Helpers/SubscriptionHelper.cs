0e35760790ceb43cb6e1367c646e436d1350293d
Shoro749 <romanbro749@gmail.com>
2026-04-15 21:46:42 +0300

    subscription seeder fixes

--- StreamingService/Helpers/SubscriptionHelper.cs
+++ StreamingService/Helpers/SubscriptionHelper.cs
@@ -6,21 +6,21 @@ public static class SubscriptionHelper
     {
         return subscriptionLevel switch
         {
-            "Premium" => "text-[#9333EA]",
-            "Standard" => "text-[#D0F260]",
-            "Basic" => "text-white",
+            "Преміум" => "text-[#9333EA]",
+            "Стандартний" => "text-[#D0F260]",
+            "Безкоштовний" => "text-white",
             _ => "text-white/50"
         };
     }
 
-    public static string GetDisplayName(string? subscriptionLevel)
-    {
-        return subscriptionLevel switch
-        {
-            "Premium" => "Преміум",
-            "Standard" => "Стандарт",
-            "Basic" => "Базовий",
-            _ => "Безкоштовно"
-        };
-    }
+    //public static string GetDisplayName(string? subscriptionLevel)
+    //{
+    //    return subscriptionLevel switch
+    //    {
+    //        "Premium" => "Преміум",
+    //        "Standard" => "Стандарт",
+    //        "Basic" => "Базовий",
+    //        _ => "Безкоштовно"
+    //    };
+    //}
 }
